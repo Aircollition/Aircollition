c74903da86fd4b6908bf6878c42694fc8539cd95
FelipeGarcia <felipe.a.garcia.s@gmail.com>
2016-05-27 11:40:32 +0200

    Moved Doc files

--- Docs/Useful python/example_name_binding.py
+++
@@ -1,9 +0,0 @@
-x = [4, 2, 10, 9, "toto"]
-y = x  # y est juste une reference au tableau x, pas de copie effectuee ici
-y[2] = "tintin"
-print x # Cela affiche  [4, 2, "tintin", 9, "toto"]
-x = [4, 2, 10, 9, "toto"]
-y = x[:]   # On demande une copie
-y[2] = "tintin"
-print x # Cela affiche  [4, 2, 10, 9, "toto"]
-print y # Cela affiche  [4, 2, "tintin", 9, "toto"]
